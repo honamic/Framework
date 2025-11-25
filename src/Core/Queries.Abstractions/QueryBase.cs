abecd6a22d8652d94d1f67c55446718c6bd84121
Iman Mohammadi <mohammadi4net@gmail.com>
2025-11-25 12:13:27 +0330

    Remove unused properties from AggregateQueryBase

--- src/Core/Queries.Abstractions/QueryBase.cs
+++ src/Core/Queries.Abstractions/QueryBase.cs
@@ -4,6 +4,4 @@ namespace Honamic.Framework.Queries;
 public class AggregateQueryBase<TKey> : EntityQueryBase<TKey>
 {
     public long Version { get; set; }
-    public string? ModifiedSources { get; set; }
-    public string? CreatedSources { get; set; }
 }
